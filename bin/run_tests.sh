a39905a4bb5a79c650d834a00fb9542219842475
Jonathan "Duke" Leto <jonathan@leto.net>
2013-09-26 14:11:30 -0700

    Exit with the proper error code when a JSON file is invalid

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
 for i in ./study/0/*.json; do
-    echo "Validating ./study/0/$i"
-    cat $i | python -mjson.tool > /dev/null
-    if [ $? -ne 0 ]; then
-        exit $?
-    fi
+    echo "Validating $i"
+    python -mjson.tool $i /dev/null || exit $?
 done
